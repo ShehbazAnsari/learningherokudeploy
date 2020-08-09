8879fc394b1eac1692a5b7e545705f1ecb279a4a
Shehbazjs <reactjs5622@gmail.com>
2020-08-09 14:34:05 +0530

    Boom

--- server.js
+++ server.js
@@ -3,20 +3,20 @@ const app = express()
 const groupJson = {
   Shehbaz: "Java Developer",
   Rafiaan: "Python Developer",
-  Adnan:"ReactNative Developer",
-  Ejaz:"Reactjs Developer",
-  Shamuwel:"TPO Developer",
-  "Bilal":"Rpa Developer"
+  Adnan: "ReactNative Developer",
+  Ejaz: "Reactjs Developer",
+  Shamuwel: "TPO Developer",
+  "Bilal": "Rpa Developer"
 }
 app.use(express.json())
-app.get('/' ,(req,res)=>{
-  res.send('<h1>First Heroku Deployment</h1>')
+app.get('/', (req, res) => {
+  res.send('First Heroku Deployment')
 })
 
-app.get('/group', (req,res)=>{
+app.get('/group', (req, res) => {
   res.json(groupJson)
 })
 
 
 const PORT = 5000
-app.listen(PORT, ()=> `PORT is succesfully running on ${PORT}`)
+app.listen(PORT, () => `PORT is succesfully running on ${PORT}`)
